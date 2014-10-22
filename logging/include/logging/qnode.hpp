d1e408500acfb240100ca1253f58d0f4fdd6c1c8
Maha-A <100035128@kustar.ac.ae>
2014-10-22 00:15:10 -0700

    initial logging

---
+++ logging/include/logging/qnode.hpp
@@ -0,0 +1,76 @@
+/**
+ * @file /include/logging/qnode.hpp
+ *
+ * @brief Communications central!
+ *
+ * @date February 2011
+ **/
+/*****************************************************************************
+** Ifdefs
+*****************************************************************************/
+
+#ifndef logging_QNODE_HPP_
+#define logging_QNODE_HPP_
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <ros/ros.h>
+#include <string>
+#include <QThread>
+#include <QStringListModel>
+#include <sensor_msgs/Joy.h>
+#include <geometry_msgs/Twist.h>
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace logging {
+
+/*****************************************************************************
+** Class
+*****************************************************************************/
+
+class QNode : public QThread {
+    Q_OBJECT
+public:
+	QNode(int argc, char** argv );
+	virtual ~QNode();
+	bool init();
+	bool init(const std::string &master_url, const std::string &host_url);
+	void run();
+    ros::Subscriber topic;
+    void topiccallback(const geometry_msgs::Twist::ConstPtr& top);
+
+
+
+	/*********************
+	** Logging
+	**********************/
+	enum LogLevel {
+	         Debug,
+	         Info,
+	         Warn,
+	         Error,
+	         Fatal
+	 };
+
+	QStringListModel* loggingModel() { return &logging_model; }
+	void log( const LogLevel &level, const std::string &msg);
+
+Q_SIGNALS:
+	void loggingUpdated();
+    void rosShutdown();
+
+private:
+	int init_argc;
+	char** init_argv;
+	ros::Publisher chatter_publisher;
+    QStringListModel logging_model;
+};
+
+}  // namespace logging
+
+#endif /* logging_QNODE_HPP_ */
